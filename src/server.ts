21cd74e0f4958927fd6660995d9326d14ce14648
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2024-01-18 01:43:02 -0300

    Arquivos base - Ajustes inicias do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express, { json } from 'express'
+import express, { json } from 'express';
 
 const app = express();
 
 app.use(json());
 
-app.listen(3333, () => console.log('Passei aqui 🐱‍🏍, App is Running at port 3333!'))
+app.listen(3333, () =>
+  console.log('Passei aqui 🐱‍🏍, App is Running at port 3333!'),
+);
