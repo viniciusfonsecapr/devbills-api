e638f210821f86043a13e6a27c931894af1e84a3
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2024-01-20 23:58:43 -0300

    Novos itens

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { json } from 'express';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(json());
+app.use(routes);
 
 app.listen(3333, () =>
   console.log('Passei aqui 🐱‍🏍, App is Running at port 3333!'),
